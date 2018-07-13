66636b9fe1d3549e69c9de99da996f6ac3906298
Isuru Fernando <isuruf@gmail.com>
2018-07-12 19:00:48 -0600

    Run autogen.sh and remove CFLAGS, CXXFLAGS, LDFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,11 +12,7 @@ if [ $(uname) == Linux ]; then
     XWIN_ARGS="--enable-xcb-shm"
 fi
 
-# Most other autotools-based build systems add
-# prefix/include and prefix/lib automatically!
-export CFLAGS=${CFLAGS}" -I${PREFIX}/include"
-export CXXFLAGS=${CXXFLAGS}" -I${PREFIX}/include"
-export LDFLAGS=${LDFLAGS}" -L${PREFIX}/lib"
+bash autogen.sh
 
 ./configure \
     --prefix="${PREFIX}" \
