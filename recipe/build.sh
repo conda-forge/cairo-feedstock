d46fe1aef7970b902e9adddb2b55ee658e8a891f
Peter Williams <peter@newton.cx>
2018-07-20 12:25:52 -0400

    Stop packaging Libtool `.la` files.
    Pursuant to https://github.com/conda-forge/staged-recipes/issues/673 and
    https://github.com/conda-forge/libxcb-feedstock/pull/9, we're going to remove
    these files from our packages. Eventually conda-build will do this
    automatically for us, but for now we take care of it manually. We need to
    delete the files both before the build (because inconsistent complements of
    `.la` files can break the build) and after (because we may have installed new
    ones).

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,6 +18,11 @@ export CFLAGS=${CFLAGS}" -I${PREFIX}/include"
 export CXXFLAGS=${CXXFLAGS}" -I${PREFIX}/include"
 export LDFLAGS=${LDFLAGS}" -L${PREFIX}/lib"
 
+# Cf. https://github.com/conda-forge/staged-recipes/issues/673, we're in the
+# process of excising Libtool files from our packages. Existing ones can break
+# the build while this happens.
+find $PREFIX -name '*.la' -delete
+
 ./configure \
     --prefix="${PREFIX}" \
     --enable-warnings \
@@ -33,3 +38,7 @@ make -j${CPU_COUNT}
 # Hangs for > 10 minutes on Linux
 #make check -j${CPU_COUNT}
 make install -j${CPU_COUNT}
+
+# Remove any new Libtool files we may have installed. It is intended that
+# conda-build will eventually do this automatically.
+find $PREFIX -name '*.la' -delete
