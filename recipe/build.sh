90c61baa172993babfd56c37cfbd670ae88b87be
Peter Williams <peter@newton.cx>
2018-07-28 19:30:23 -0400

    Re-enable X11 components on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# As of Mac OS 10.8, X11 is no longer included by default
-# (See https://support.apple.com/en-us/HT201341 for the details).
-# Due to this change, we disable building X11 support for cairo on OS X by
-# default.
-
-if [ $(uname) == Darwin ]; then
-    XWIN_ARGS="--disable-xlib --disable-xcb --disable-glitz"
-fi
-if [ $(uname) == Linux ]; then
-    XWIN_ARGS="--enable-xcb-shm"
-fi
-
 # Most other autotools-based build systems add
 # prefix/include and prefix/lib automatically!
 export CFLAGS=${CFLAGS}" -I${PREFIX}/include"
@@ -31,7 +19,7 @@ find $PREFIX -name '*.la' -delete
     --enable-pdf \
     --enable-svg \
     --disable-gtk-doc \
-    $XWIN_ARGS
+    --enable-xcb-shm
 
 make -j${CPU_COUNT}
 # FAIL: check-link on OS X
